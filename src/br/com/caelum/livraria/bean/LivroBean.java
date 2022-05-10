3d5605540e1de1e4f9806cfc9d2635a35e84d6f4
taian10 <taianmendes@hotmail.com>
2022-05-10 13:43:06 -0300

    projeto-livraria

---
+++ src/br/com/caelum/livraria/bean/LivroBean.java
@@ -0,0 +1,21 @@
+package br.com.caelum.livraria.bean;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class LivroBean {
+
+	private Livro livro = new Livro();
+
+	
+	public Livro getLivro() {
+		return livro;
+	}
+
+	public void gravar() {
+		System.out.println("Gravando livro " + this.livro.getTitulo());
+	}
+
+
+
+}
